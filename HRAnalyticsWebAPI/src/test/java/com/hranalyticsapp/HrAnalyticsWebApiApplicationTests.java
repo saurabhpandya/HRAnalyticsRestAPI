dace24bf53cc72fd1bb77079e378dfcb4362eb99
saurabh <saurabhpandya7@gmail.com>
2020-06-15 18:57:04 +0530

    Initial commit

---
+++ HRAnalyticsWebAPI/src/test/java/com/hranalyticsapp/HrAnalyticsWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hranalyticsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAnalyticsWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
